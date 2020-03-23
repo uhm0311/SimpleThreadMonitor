6930530c7728059dcdf9abe1f537bca63150ba92
uhm0311 <uhm0311@naver.com>
2020-03-23 18:54:13 +0900

    Create project.

---
+++ ThreadMonitor/Mutex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadMonitor
+{
+    public static class Mutex
+    {
+        public static void Lock(object Object, Action Process, Action<Exception> ExceptionCallback = null, bool ReleaseLockBeforeExceptionCallback = false)
+        {
+            if (Object != null && Process != null)
+            {
+                try
+                {
+                    Monitor.Enter(Object);
+                    Process();
+                }
+                catch (Exception Exception)
+                {
+                    if (ReleaseLockBeforeExceptionCallback)
+                    {
+                        Monitor.Exit(Object);
+                    }
+
+                    ExceptionCallback?.Invoke(Exception);
+                }
+                finally
+                {
+                    if (!ReleaseLockBeforeExceptionCallback)
+                    {
+                        Monitor.Exit(Object);
+                    }
+                }
+            }
+        }
+    }
+}
